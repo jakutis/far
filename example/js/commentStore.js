7c8d547383e5161f600ff3b98df07f9f28cdc425
Vytautas Jakutis <jakutis@wix.com>
2014-11-12 17:45:19 +0200

    example: clean backend commentStore code

--- example/js/commentStore.js
+++ example/js/commentStore.js
@@ -1,54 +1,30 @@
-var _ = require('lodash');
 global.app.factory('commentStore', function(guid, EventEmitter, dispatcher, actionTypes) {
-    function CommentStore() {
-        EventEmitter.call(this);
-        this.comments = [
-            {
-                guid: guid(),
-                content: 'This is awesome'
-            },
-            {
-                guid: guid(),
-                content: 'I agree'
-            },
-            {
-                guid: guid(),
-                content: 'First'
-            }
-        ];
-    }
-    CommentStore.prototype = _.create(EventEmitter.prototype, {
-        'constructor': CommentStore
-    });
-    _.assign(CommentStore.prototype, {
-        comments: null,
-        addComment: function(comment) {
-            if(this.comments.every(function(c) {
-                return c.guid !== comment.guid;
-            })) {
-                this.comments.push(comment);
-            }
+    var comments = [
+        {
+            guid: guid(),
+            content: 'This is awesome'
         },
-        emitChange: function() {
-            this.emit('change');
+        {
+            guid: guid(),
+            content: 'I agree'
+        },
+        {
+            guid: guid(),
+            content: 'First'
         }
-    });
-
-    var commentStore = new CommentStore();
+    ];
     dispatcher.addListener(function(action) {
         if(action.actionType === actionTypes.ADD_COMMENT) {
-            commentStore.addComment(action.item);
+            if(comments.every(function(c) {
+                return c.guid !== action.item.guid;
+            })) {
+                comments.push(action.item);
+            }
         }
-        commentStore.emitChange();
     });
     return {
-        addListener: function(l) {
-            commentStore.addListener(l);
-        },
         getComments: function() {
-            return commentStore.comments.slice();
+            return comments.slice();
         }
     };
 });
-
-
